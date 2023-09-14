cf26b0a76fcf64a2e252f38ff1a292b5e3b3b0fa
Ed Sweeney <ed@onextent.com>
2023-09-13 18:12:59 -0700

    test for prerix passing

--- tests/prefix_parsing.rs
+++ tests/prefix_parsing.rs
@@ -7,8 +7,13 @@ fn test_prefix(input: &str) {
     let result = parser.parse(input);
     assert!(result.is_ok(), "Parse failed with: {:?}", result);
     assert_eq!(
-        result.clone().unwrap(),
-        ("myns".to_string(), "http://example.com/myns#".to_string()),
+        result.clone().unwrap().0,
+        "myns".to_string(),
+        "Parse failed with: {:?}",
+        result
+    );
+    assert!(
+        result.clone().unwrap().1.ends_with("://example.com/myns#"),
         "Parse failed with: {:?}",
         result
     );
@@ -18,48 +23,10 @@ fn test_prefix(input: &str) {
 fn test_prefix_basic() {
     let input = "@prefix myns: <http://example.com/myns#> .";
     test_prefix(input);
-    // let parser = prefix::PrefixParser::new();
-    // let result = parser.parse(input);
-    // assert!(result.is_ok(), "Parse failed with: {:?}", result);
-    // assert_eq!(
-    //     result.clone().unwrap(),
-    //     ("myns".to_string(), "http://example.com/myns#".to_string()),
-    //     "Parse failed with: {:?}",
-    //     result
-    // );
+    let input = "@prefix myns: <https://example.com/myns#> .";
+    test_prefix(input);
+    let input = "@prefix myns:<https://example.com/myns#> .";
+    test_prefix(input);
+    let input = "@prefix myns:<https://example.com/myns#>.";
+    test_prefix(input);
 }
-// #[test]
-// fn test_prefix_parsing() {
-//     // Valid input
-//     //let input = "@prefix myns: <http://example.com/myns#> .";
-//     //let input = "@prefix ns: http .";
-//     let input = "http";
-//     match prefix::PrefixParser::new().parse(input) {
-//         Ok((ns, uri)) => {
-//             assert_eq!(ns, "ns");
-//             assert_eq!(uri, "http://example.com/ns#");
-//         }
-//         d => assert!(false, "Failed to parse valid input: {:?}", d),
-//     }
-//
-//     // Optionally, you can add more checks for valid inputs.
-//
-//     // Test for invalid input
-//     let invalid_input = "@prefix ns http://example.com/ns# .";
-//     assert!(
-//         prefix::PrefixParser::new().parse(invalid_input).is_err(),
-//         "Invalid input was wrongly accepted"
-//     );
-//
-//     // Test for input with different whitespace patterns
-//     let input_with_spaces = "@prefix    ns  :    <http://example.com/ns#>    .";
-//     match prefix::PrefixParser::new().parse(input_with_spaces) {
-//         Ok((ns, uri)) => {
-//             assert_eq!(ns, "ns");
-//             assert_eq!(uri, "http://example.com/ns#");
-//         }
-//         _ => assert!(false, "Failed to parse input with spaces"),
-//     }
-//
-//     // ... And you can add more test cases as needed
-// }
