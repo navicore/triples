81e724ca6bff5aae9a025302a2314eb68a9842a8
Ed Sweeney <ed@onextent.com>
2023-09-25 11:27:55 -0700

    isa

--- tests/turtle_body_parsing.rs
+++ tests/turtle_body_parsing.rs
@@ -1,5 +1,7 @@
+use triples::data::RdfName;
 use triples::turtle::LineParser;
 use triples::turtle::RDFNameParser;
+use triples::turtle_stream::ParsedLine;
 
 #[test]
 fn test_subject_as_uri_parsing() {
@@ -32,6 +34,19 @@ fn test_isa_parsing() {
     let parser = LineParser::new();
     let input = "<http://cmu.edu/building/ontology/ghc#8Floor> a brick:Floor .";
     let parsed1 = parser.parse(input).unwrap();
+    if let ParsedLine::SubjectPredObjTerm(spre, subj, predpre, pred, opre, obj) = parsed1 {
+        assert!(spre.is_none());
+        assert_eq!(
+            subj,
+            RdfName::new("http://cmu.edu/building/ontology/ghc#8Floor".to_string())
+        );
+        assert!(predpre.is_some());
+        assert_eq!(pred, RdfName::new("type".to_string()));
+        assert!(opre.is_some());
+        assert_eq!(obj, "Floor".to_string());
+    } else {
+        assert!(false)
+    };
     // assert_eq!(
     //     parsed1,
     //     (
