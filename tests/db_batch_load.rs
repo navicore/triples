74eb8743e1d9bbc554ac8f7de878201ff9c86c43
Ed Sweeney <ed@onextent.com>
2023-09-25 19:29:09 -0700

    todo

--- tests/db_batch_load.rs
+++ tests/db_batch_load.rs
@@ -41,3 +41,41 @@ async fn test_ttl_to_db() {
     assert_eq!(subject_names.len(), 33);
     tx.commit().await.unwrap();
 }
+
+//#[tokio::test]
+async fn test_bricks_to_db() {
+    let path = Path::new("tests/data/bricks_ex1.ttl");
+    let file = File::open(&path).expect("Failed to open file");
+    let reader = io::BufReader::new(file);
+
+    let mut stream = TurtleStream::new();
+
+    const TEST_DB_FILE: &str = "/tmp/triples_bricks_load_test.db";
+    let db_api = DbApi::new(TEST_DB_FILE.to_string()).await.unwrap();
+    let tx = db_api.begin_txn().await.unwrap();
+
+    for line in reader.lines() {
+        let line = line.expect("Failed to read a line");
+        if line.len() == 0 {
+            continue;
+        }
+        match stream.load(&line) {
+            Ok(r) => {
+                match r {
+                    Some(subject) => {
+                        // insert into db
+                        db_api.insert(&subject).await.expect("Insert failed");
+                    }
+                    _ => {} // noop
+                };
+            }
+            Err(e) => {
+                assert!(false, "error: {} on input: {}", e, line)
+            }
+        }
+    }
+
+    let subject_names = db_api.query_all_subject_names().await.unwrap();
+    assert_eq!(subject_names.len(), 42);
+    tx.commit().await.unwrap();
+}
